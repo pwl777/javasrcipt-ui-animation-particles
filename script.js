9c9c92c59499565c50b8783934bd5eeb50e31317
Pierson Lippard <68010303+pwl777@users.noreply.github.com>
2021-07-13 23:04:48 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,104 @@
+/* ------ JavaScript - UI Animation - Particles ------ */
+
+/* =============== Get DOM Elements =============== */
+
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+/* =============== Measuring Buttons =============== */
+
+const buttonElements = document.querySelectorAll(".button");
+let buttonMeasurements = [];
+function measureButtons() {
+  buttonMeasurements = [];
+  buttonElements.forEach((button) => {
+    buttonMeasurements.push(button.getBoundingClientRect());
+  });
+}
+measureButtons();
+console.log(buttonMeasurements);
+
+/* =============== Create Particles =============== */
+
+let particlesArray = [];
+class Particle {
+  constructor(x, y, size) {
+    this.x = x;
+    this.y = y;
+    this.size = size;
+    this.weight = Math.random() * 1.5 + 1.5;
+    this.directionX = Math.random() * 5;
+  }
+  update() {
+    this.y += this.weight; // This will make particles move upwards.
+    // Need to make sure that the particles size is above 0.
+    if (this.size >= 0.3) this.size -= 0.3;
+  }
+  draw() {
+    ctx.fillStyle = "white";
+    ctx.fillRect(this.x, this.y, this.size, this.size);
+  }
+}
+
+let activeButton = -1;
+buttonElements.forEach((button) =>
+  button.addEventListener("mouseenter", function () {
+    activeButton = button.dataset.number;
+  })
+);
+buttonElements.forEach((button) =>
+  button.addEventListener("mouseleave", function () {
+    activeButton = -1;
+  })
+);
+
+function handleParticles() {
+  for (let i = 0; i < particlesArray.length; i++) {
+    for (let y = i; y < particlesArray.length; y++) {
+      let dx = particlesArray[i].x - particlesArray[y].x;
+      let dy = particlesArray[i].y - particlesArray[y].y;
+      let distance = Math.sqrt(dx * dx + dy * dy);
+      let opacity = particlesArray[i].size / 80 + particlesArray[y].size / 80;
+      ctx.strokeStyle = "rgba(255, 255, 255," + opacity;
+      if (distance < 90) {
+        ctx.beginPath();
+        ctx.moveTo(particlesArray[i].x, particlesArray[i].y);
+        ctx.lineTo(particlesArray[y].x, particlesArray[y].y);
+        ctx.stroke();
+      }
+    }
+
+    particlesArray[i].update();
+    particlesArray[i].draw();
+    if (particlesArray[i].size <= 1) {
+      particlesArray.splice(i, 1);
+      i--;
+    }
+  }
+}
+function createParticle() {
+  if (activeButton > -1) {
+    let size = Math.random() * 40 + 7; // 40 + 10;
+    let x =
+      Math.random() * (buttonMeasurements[activeButton].width - size * 2) +
+      buttonMeasurements[activeButton].x +
+      size;
+    let y = buttonMeasurements[activeButton].y + 40;
+    particlesArray.push(new Particle(x, y, size));
+  }
+}
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  createParticle();
+  handleParticles();
+  requestAnimationFrame(animate);
+}
+animate();
+
+window.addEventListener("resize", function () {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  measureButtons();
+});
